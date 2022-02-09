6246316d60773e26412736d4427a75da6b3cdf47
anyang <anyang_zhu@126.com>
2022-02-09 21:36:14 +0800

    leetcode 9

--- 9.回文数.java
+++ 9.回文数.java
@@ -8,29 +8,18 @@
 class Solution {
 
     public boolean isPalindrome(int x) {
-        // 负数不是回文数
-        if(x < 0) 
+        // 负数不是回文数, 并且末位为 0 也不是回文数，因为第一位（从左向右）无法为 0
+        if(x < 0 || (x != 0 && x % 10 == 0)) {
             return false;
-        int num = 0;
-        while(x != 0) {
-            num = num * 10 + x % 10;
-            if(num >= x) {
-                break;
-            }
-            x = x / 10;
         }
-        System.out.println("num is " + num);
-        System.out.println("x is " + x);
-
-
-        return num == x;
-    }
-
-    public static void main(String[] args) {
-        Integer num = 121;
-        Solution solution = new Solution();
-        boolean isPalindrome = solution.isPalindrome(num);
-        System.out.println("is " + num + "palindrome? " + isPalindrome);
+        int y = 0;
+        while(y < x) {
+            y = y * 10 + x % 10;
+            x = x / 10;        
+        }
+        // 如果x长度为偶数位，应该相等
+        // 如果x长度为奇数位，因为 x 已经除以10了，所以应该和 y 除以10相等
+        return y == x || y / 10 == x;
     }
 }
 // @lc code=end
